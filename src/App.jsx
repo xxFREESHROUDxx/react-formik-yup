01e5042179600fad073647c48d1d53699010e3c1
xxFREESHROUDxx <baibhavkc11@gmail.com>
2023-05-16 15:29:38 +0545

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='App mt-60'>
+      <form className='border border-gray-400 p-6'>
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='name' className='text-xl font-semibold'>
+            Name
+          </label>
+          <input
+            className='bg-gray-100 border w-[458px] h-[40px] border-black rounded'
+            type='text'
+            id='name'
+            name='name'
+          />
+        </div>
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='email' className='text-xl font-semibold'>
+            E-mail
+          </label>
+          <input
+            className='bg-gray-100 border h-[40px] border-black rounded'
+            type='email'
+            id='email'
+            name='email'
+          />
+        </div>
+        <div className='flex flex-col mb-5'>
+          <label htmlFor='channel' className='text-xl font-semibold'>
+            Channel
+          </label>
+          <input
+            className='bg-gray-100 border h-[40px] border-black rounded'
+            type='text'
+            id='channel'
+            name='channel'
+          />
+        </div>
+
+        <button
+          type='submit'
+          className='bg-green-500 mt-4 text-white px-4 py-2 rounded font-medium hover:bg-green-600'
+        >
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
